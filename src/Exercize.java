58c222f8e41008626125299815f282232cbea862
redwolf <lisenkov0a@gmail.com>
2022-10-25 20:57:32 +0700

    First exercize solved

---
+++ src/Exercize.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+public class Exercize {
+
+    public static void exercize1 (){
+        // Pi with two characters
+        double result = Math.PI;
+        System.out.println(String.format("%.2f", result));
+    }
+    public static void main(String[] args) {
+        exercize1();
+    }
+}
